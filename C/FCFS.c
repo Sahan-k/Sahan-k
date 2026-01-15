d8b7ee66d52ec5443aba91780f5e77dfc6ffd0c8
Sahan-k <209514159+Sahan-k@users.noreply.github.com>
2026-01-15 23:18:56 +0530

    Adding Comments to the program

--- C/FCFS.c
+++ C/FCFS.c
@@ -1,3 +1,4 @@
+// The program is for First come forst serve scheduling and also calculate TAT,ATAT,WT,AWT.
 #include<stdio.h>
 int main()
 {
@@ -7,24 +8,25 @@ int main()
    scanf("%d",&n);
    for(i=1;i<=n;i++)
    {
-     printf("Enter the process: ");
+     printf("Enter the process: ");  
      scanf("%d",&a[i]);
 
      printf("Enter the Burst time: ");
      scanf("%d",&b[i]);
-     if(i<=n-1)
+     if(i<=n-1)                      // calculate waiting time 
      {
-      Time+=b[i];
-      WT+=Time;
-     }
-     total+=b[i];
-     TAT+=total;
+      Time+=b[i];                   // use one varibale to store the sum of burst time
+      WT+=Time;                     // use one varibale to store the WT 
+     } 
+     total+=b[i];                   //total variable used to store the sum of burst time
+     TAT+=total;                    // TAT variable used to store the TAT
    }
    
-   printf("Total TAT is %.2f\n",TAT);
-   ATAT=TAT/n;
-   printf("Average TAT is %.2fms\n",ATAT);
-   printf("Waiting time is %.2f\n",WT);
-   AWT=WT/n;
-   printf("Avereage WT is %.2f\n",AWT);
+   printf("Total TAT is %.2f\n",TAT);      //prints the TAT with a 2 decimal part
+   ATAT=TAT/n;                             // Calculate Average turn around time
+   printf("Average TAT is %.2fms\n",ATAT);  // Prints the value of TAT
+   printf("Waiting time is %.2f\n",WT);      //Prints the WT with 2 decimal part
+   AWT=WT/n;                                // Caclulate average Waiting time 
+   printf("Avereage WT is %.2f\n",AWT);     //Prints Average Waiting time with 2 decimal part
 }
+
